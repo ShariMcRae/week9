790980922e3c9adb0e52bd7c6e96c6a0dd9fb882
Shari McRae <sharimcrae@protonmail.com>
2023-02-14 20:57:07 -0600

    adding code to repository

---
+++ menu.js
@@ -0,0 +1,16 @@
+document.write(`
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.html">Students</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controle="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="index.html">List</a></li>
+                <li class="nav-item"><a class="nav-link" href="add.html">Add</a></li>
+                <li class="nav-item"><a class="nav-link" href="delete.html">Delete</a></li>
+            </ul>
+        </div>
+    </nav>
+`);
